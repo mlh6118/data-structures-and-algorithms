dfda6db0d4539e8935c5eecdee4d51a062cb30c0
Marni Hager <mlh6118@gmail.com>
2022-08-28 16:07:48 -0700

    Wrote basic code.

--- python/code_challenges/hashmap-repeated-word/hashmap_repeated_word.py
+++ python/code_challenges/hashmap-repeated-word/hashmap_repeated_word.py
@@ -1,4 +1,7 @@
-def repeated_word(phrase):
+import re
+from hashtable import Hashtable
+
+def repeated_word(phrase: str):
     # In Plain English:
     # Split phrase into individual words.
     # Check if hash table contains next word.
@@ -12,4 +15,20 @@ def repeated_word(phrase):
     # 2a. Check hash table for word
     # 2b. If true, return word
     # 2c. If not, add word to table
+
+    list_of_words = re.split(r' |,|;|\.|\?|!|:|-', phrase.lower())
+    print(list_of_words)
+
+    hashtable_of_words = Hashtable()
+
+    for word in list_of_words:
+        if hashtable_of_words.contains(word):
+            return word
+        hashtable_of_words.set(word, word)
+
+        print(hashtable_of_words.keys())
+
+
+if __name__ == '__main__':
+    print(repeated_word("It was the best of times, it was the worst of times."))
 
