0361645da7b9cbeb869d146e75ad196919dc48d5
Marni Hager <mlh6118@gmail.com>
2022-08-28 15:36:06 -0700

    Adding plain English and Algorithm for future reference.

---
+++ python/code_challenges/hashmap-repeated-word/hashmap_repeated_word.py
@@ -0,0 +1,15 @@
+def repeated_word(phrase):
+    # In Plain English:
+    # Split phrase into individual words.
+    # Check if hash table contains next word.
+    # If word already in hash table, return word.
+    # If not, put word in hash table
+    # Go to next word and check if in hash table.
+
+    # Algorithm:
+    # 1. Split phrase
+    # 2. Loop over words
+    # 2a. Check hash table for word
+    # 2b. If true, return word
+    # 2c. If not, add word to table
+
